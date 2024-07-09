991282f8245db2531e050a73a3754800cd22ce80
Bharath9620 <bharath.n@ergobite.com>
2024-07-09 18:33:44 +0530

    Update provider.tf

--- VPC/provider.tf
+++ VPC/provider.tf
@@ -1,6 +1,6 @@
 provider "aws" {
   region     = "ap-south-1a"
-  access_key = "" #Access_key for the AWS account
-  secret_key = "" #Access_key for the AWS account
+  access_key = "AWS_ACCESS_KEY_ID" #Access_key for the AWS account
+  secret_key = "AWS_SECRET_ACCESS_KEY" #Access_key for the AWS account
  
 }
