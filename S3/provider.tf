11aef5b5e9aeeb9fe8934d990d27b2c8419e613f
Bharath9620 <bharath.n@ergobite.com>
2024-07-09 18:32:59 +0530

    Update provider.tf

--- S3/provider.tf
+++ S3/provider.tf
@@ -1,5 +1,5 @@
 provider "aws" {
   region = "ap-south-1a"
-  access_key = ""      #access-key of our AWS account
-  secret_key = ""      #secret_key of our AWS account 
+  access_key = "AWS_ACCESS_KEY_ID"      #access-key of our AWS account
+  secret_key = "AWS_SECRET_ACCESS_KEY"      #secret_key of our AWS account 
 }
